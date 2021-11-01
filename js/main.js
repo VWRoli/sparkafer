1fb0407d05d6da8b6fe314cf46cec49013651ab7
VWRoli <fustroli@gmail.com>
2021-11-01 09:22:10 +0100

    feat: add new pictures

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 // VARIABLES
-const container = document.querySelector(".images-container");
-const overlayEl = document.querySelector(".overlay");
-const modalEl = document.querySelector(".modal");
-const nextArrowEl = document.querySelector(".fa-arrow-circle-right");
-const prevArrowEl = document.querySelector(".fa-arrow-circle-left");
+const container = document.querySelector('.images-container');
+const overlayEl = document.querySelector('.overlay');
+const modalEl = document.querySelector('.modal');
+const nextArrowEl = document.querySelector('.fa-arrow-circle-right');
+const prevArrowEl = document.querySelector('.fa-arrow-circle-left');
 
 const startImgNumber = 1;
-const endImgNumber = 93;
+const endImgNumber = 106;
 const imageNumbers = [];
 let currentImgNumber = 1;
 
@@ -27,9 +27,9 @@ for (let i = startImgNumber; i <= endImgNumber; i++) {
 
 imageNumbers.forEach((imgNumber) => {
   const html = `<div class="image-wrapper">
-    <img src="./images/thumbnails/image_${imgNumber}.jpg" data-nr="${imgNumber}" alt="Image ${imgNumber}" />
+    <img src="./images/image_${imgNumber}.jpg" data-nr="${imgNumber}" alt="Image ${imgNumber}" />
     </div>`;
-  container.insertAdjacentHTML("beforeend", html);
+  container.insertAdjacentHTML('beforeend', html);
 });
 
 /////////////////////////////////////////////////
@@ -38,37 +38,37 @@ imageNumbers.forEach((imgNumber) => {
 // SHOW MODAL
 
 const toggleModal = () => {
-  if (modalEl.classList.contains("visible")) {
+  if (modalEl.classList.contains('visible')) {
     clear();
   } else {
     setImage(currentImgNumber);
   }
-  overlayEl.classList.toggle("visible");
-  modalEl.classList.toggle("visible");
+  overlayEl.classList.toggle('visible');
+  modalEl.classList.toggle('visible');
 };
 
 const setImage = () => {
   clear();
-  const html = `<img src="./images/img/image_${currentImgNumber}.jpg" alt="Image ${currentImgNumber}" />`;
-  modalEl.insertAdjacentHTML("afterbegin", html);
+  const html = `<img src="./images/image_${currentImgNumber}.jpg" alt="Image ${currentImgNumber}" />`;
+  modalEl.insertAdjacentHTML('afterbegin', html);
 };
 
 //Open modal clicking on images
-container.addEventListener("click", (e) => {
+container.addEventListener('click', (e) => {
   const clicked = e.target.localName;
   currentImgNumber = e.target.dataset.nr;
 
-  if (clicked !== "img") return;
+  if (clicked !== 'img') return;
 
   toggleModal(currentImgNumber);
 });
 
 //close modal with ovarlay click
-overlayEl.addEventListener("click", toggleModal);
+overlayEl.addEventListener('click', toggleModal);
 
 //Close modal window with Esc
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && modalEl.classList.contains("visible"))
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalEl.classList.contains('visible'))
     toggleModal();
 });
 
@@ -84,13 +84,13 @@ const prevImg = () => {
 };
 
 // NEXT ARROW
-modalEl.addEventListener("click", (e) => {
+modalEl.addEventListener('click', (e) => {
   const clicked = e.target;
 
-  if (!clicked.classList.contains("fas")) return;
+  if (!clicked.classList.contains('fas')) return;
 
-  if (clicked.classList.contains("fa-arrow-circle-left")) prevImg();
-  if (clicked.classList.contains("fa-arrow-circle-right")) nextImg();
+  if (clicked.classList.contains('fa-arrow-circle-left')) prevImg();
+  if (clicked.classList.contains('fa-arrow-circle-right')) nextImg();
 });
 // PREV ARROW
-prevArrowEl.addEventListener("click", prevImg);
+prevArrowEl.addEventListener('click', prevImg);
